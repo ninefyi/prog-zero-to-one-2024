d44f1eec1d7eba26abaff6e4657ed800008b3109
Piti.C <276376+ninefyi@users.noreply.github.com>
2024-01-21 19:06:54 +0800

    update hw

--- week2/hw2.rkt
+++ week2/hw2.rkt
@@ -1,21 +1,13 @@
 #lang racket
 (require 2htdp/image)
 
-(define (area-circle r)
-  (* pi (sqr r)))
-
-(define  (area-rectangle w h)
-  (* w h))
-
-(define (area-square s)
-  (area-rectangle s s))
-
-(define (area-donut r1 r2)
-  (abs (- (area-circle r1)
-          (area-circle r2))))
-
 (define (half n)
   (/ n 2))
+
+(struct square (side) #:transparent)
+
+(define (aside s)
+  (* (half s) (sqrt 2)))
 
 (define (hypotenuse a b)
   (sqrt (+ (sqr a) (sqr b))))
@@ -23,56 +15,43 @@
 (define (hypotenuse-eq a)
   (hypotenuse a a))
 
-(define (area-visible-circle-parts s)
-  (- (area-circle (half s))
-     (area-square (hypotenuse-eq (half s)))))
+(define color-rec (color 0 255 255 127))
+(define color-ccl (color 255 0 0 111))
 
 (define (draw-circle r)
-  (circle r "solid" "red"))
+  (circle r "solid" color-ccl))
 (define (draw-rectangle s)
-  (rectangle s s "outline" "black"))
+  (define side (square-side s))  
+  (rectangle side side "solid" color-rec))
 
-(define c1 (draw-circle 50))
-(define r1 (draw-rectangle 100))
-(define p1 (overlay c1 r1))
-
-;(overlay
-; (rotate 45 (draw-rectangle 70))
-; (overlay
-;  (draw-circle 50)
-;  (overlay
-;   (rotate 0 (draw-rectangle 100))
-;   (overlay
-;    (draw-circle 35)
-;    (overlay
-;     (rotate 0 (draw-rectangle 50))
-;     (overlay
-;      (draw-circle 25)
-;      (rotate 45 (draw-rectangle 35))
-;      )
-;     )
-;    )
-;   )
-;  )
-; )
+(define (no-rotate rec) (rotate 0 rec))
+(define (do-rotate rec) (rotate 45 rec))
 
 (define s 100)
-(define (aside s) (* (half s) (sqrt 2)))
 
-(define rec1 (rectangle s s "outline" "black"))
-(define rec2 (rectangle (aside s) (aside s) "outline" "black"))
-(define rec3 (rectangle (aside (aside s)) (aside (aside s)) "outline" "black"))
-(define rec4 (rectangle (aside (aside (aside s))) (aside (aside (aside s))) "outline" "black"))
+(define s1 (square s))
+(define s2 (square (aside s)))
+(define s3 (square (aside (aside s))))
+(define s4 (square (aside (aside (aside s)))))
 
-(define ccl1 (circle (half s) "outline" "red"))
-(define ccl2 (circle (half (aside s)) "outline" "red"))
-(define ccl3 (circle (half (half (aside s))) "outline" "red"))
+(define r1 (half s))
+(define r2 (half (aside s)))
+(define r3 (half (aside (aside s))))
 
-(overlay rec1
-         (overlay (rotate 45 rec2)
-                  (overlay rec3
-                           (rotate 45 rec4))))
+(define rec1 (draw-rectangle s1))
+(define rec2 (draw-rectangle s2))
+(define rec3 (draw-rectangle s3))
+(define rec4 (draw-rectangle s4))
 
-(overlay ccl1 
-         (overlay ccl2
-                  ccl3))
+(define ccl1 (draw-circle r1))
+(define ccl2 (draw-circle r2))
+(define ccl3 (draw-circle r3))
+
+(underlay (no-rotate rec1)
+          (underlay ccl1
+                    (underlay (do-rotate rec2)
+                              (underlay ccl2
+                                        (underlay (no-rotate rec3)
+                                                  (underlay ccl3
+                                                            (do-rotate rec4)))))))
+        
--- week2/hw3.rkt
+++ week2/hw3.rkt
@@ -25,5 +25,20 @@
 (define (area-visible-circle-parts s)
   (- (area-circle (half s))
      (area-square (hypotenuse-eq (half s)))))
-  
 
+(define s1 100)
+(define s2 (hypotenuse-eq (half s1)))
+(define s3 (hypotenuse-eq (half (hypotenuse-eq (half s1)))))
+
+(define a1 (area-visible-circle-parts s1))
+(define a2 (area-visible-circle-parts s2))
+(define a3 (area-visible-circle-parts s3))
+
+(+ a1 (+ a2 a3))
+
+(define (tail-sum a b s [acc 0])
+  (if (= a 1)
+      (area-visible-circle-parts s)
+      (area-visible-circle-parts (hypotenuse-eq (half s)))))
+      
+ 
